4a6cfb5a70688aef87465226906eecc0760ab249
NidaaB <bukharin@go.stockton.edu>
2023-04-26 13:30:29 -0400

    commit

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -1,19 +1,20 @@
 package HW4;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
-
 public class Arrays {
+
     static Scanner scan = new Scanner(System.in);
 
-
-    // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN OUTPUT THE 
+    // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN
+    // OUTPUT THE
     // ARRAY AND THE TOTAL.
     // SEE HW3 FOR SUMS METHOD
     // If you want to use an ArrayList for the inputs to be dynamic that is okay
-    // Otherwise if you are using a regular array, assume the size is 10 total. 
-    // EXAMPLE 
+    // Otherwise if you are using a regular array, assume the size is 10 total.
+    // EXAMPLE
     // I will add up the numbers you give me....
     // Number: 12
     // The total so far is 12.
@@ -50,10 +51,10 @@ public class Arrays {
         System.out.println("Array: " + nums);
     }
 
-
     // Create a method that will brute force a password EX.
     // bruteForce("ZZZZ")
-    // The program should then guess each char of the string and compare it to see if it is equal
+    // The program should then guess each char of the string and compare it to see
+    // if it is equal
     // to the index of the alphabet char array below
     // Output Example:
     // Z
@@ -61,17 +62,20 @@ public class Arrays {
     // ZZZ
     // ZZZZ
 
-    // Hint 1: using a char array for the password is helpful here 
-    // Hint 2: using the method .toCharArray is a way to convert the password parameter to a char array as well
+    // Hint 1: using a char array for the password is helpful here
+    // Hint 2: using the method .toCharArray is a way to convert the password
+    // parameter to
+    // a char array as well
     // From there creating for and while loops to compare the values is trivial
 
-    public static String bruteForce(String password){
-
+    public static String bruteForce(String password) {
         String answer = "";
-
-        char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
+        char[] pass = password.toCharArray();
+        char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z',
+                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '/', '?' };
 
         for (char c1 : pass) {
             for (char c2 : alphabet) {
@@ -81,17 +85,17 @@ public class Arrays {
                 }
             }
         }
-    
-        return answer; 
+
+        return answer;
     }
 
-
-    // Create a method that will sort an Array of integers from smallest to greatest.
+    // Create a method that will sort an Array of integers from smallest to
+    // greatest.
     // NOTE: You CANNOT use the built in Arrays.sort method
-    // Ex. 
+    // Ex.
     // sorter([9,10,2,5,3,4,7,8]) -> [2,3,4,5,7,8,9,10]
 
-    // Hint 1: a temp Array is a helpful way to store variables for comparing 
+    // Hint 1: a temp Array is a helpful way to store variables for comparing
     // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
     public static int[] sorter(int[] nums) {
         int LENGTH = nums.length;
@@ -117,12 +121,6 @@ public class Arrays {
         return nums;
     }
 
-
-
-
-
-
-
     public static void main(String[] args) {
         // Tester main method for your methods
         Scanner scan = new Scanner(System.in);
@@ -141,7 +139,7 @@ public class Arrays {
         System.out.println("-----------Sorting-----------");
         int[] nums = { 9, 10, 2, 5, 3, 4, 7, 8 };
         int[] sorted = sorter(nums);
-        System.out.println(Arrays.toString(sorted));
+        System.out.print("Sorted Array: ");
 
         scan.close();
 
